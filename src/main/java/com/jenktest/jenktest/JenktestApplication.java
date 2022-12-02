9df2792aa47bd18947176c1c3d57ae00d8016000
Sarabac <36499355+Sarabac@users.noreply.github.com>
2022-12-02 14:49:34 +0100

    first commit

---
+++ src/main/java/com/jenktest/jenktest/JenktestApplication.java
@@ -0,0 +1,13 @@
+package com.jenktest.jenktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenktestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jenktest/jenktest/controller/MathController.java
@@ -0,0 +1,17 @@
+package com.jenktest.jenktest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    @GetMapping("/sum/{a}/{b}")
+    public Integer sum(@PathVariable("a") final int a, @PathVariable("b") final int b) {
+        return a + b;
+    }
+
+}
